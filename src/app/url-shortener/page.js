725504e69f8f25b7f28fbd415fe486c02a84fb79
Sojith Sunny <sojithsunny29034@gmail.com>
2024-12-13 23:53:23 +0530

    shortener init

---
+++ src/app/url-shortener/page.js
@@ -0,0 +1,11 @@
+import '../globals.css';
+
+export default function Shortener() {
+    return (
+      <div className="flex flex-col items-center">
+        <h1 className="text-center text-gray-200 font-serif text-3xl font-bold my-5">URL Shortener</h1>
+        {/* Your QR Code logic here */}
+      </div>
+    );
+  }
+  
